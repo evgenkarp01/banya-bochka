acd4557282c292826ba59565405ae3581bd63438
evgenkarp01 <ewal@list.ru>
2019-10-29 23:32:17 +0300

    29102019-2

--- js/custom.js
+++ js/custom.js
@@ -2,6 +2,9 @@ $( document ).ready(function() {
 
 
 	
+    alert("ready");
+
+
 
 
 
--- js/index.js
+++ js/index.js
@@ -1,28 +1,9 @@
-import "./import/modules.js";
-
 
 $( document ).ready(function() {
-var men=true;
-	var glschet =1;
-$("#sandwich").click(function()
-	{
-		if(men==false)
-		{
-			$(".sub__menu").slideUp();
-			men=!men;
-		}
-		else
-		{
-			$(".sub__menu").slideDown();
-			men=!men;
-		}
-	});
-		$("#sandwich, .menu_item").click(function() {
-		 $("#sandwich").toggleClass("active");
-	});
-	$(".sub__menu ul li").click(this, function(){
-
-			$(this).find("ul").toggle(500);	
-	});
+    $("#btn-no-menu, #mmenu, .mmenu li a").on('click', function () {
+        $("#menuha").toggleClass("menuha-left");
+        $(".over").toggleClass("zet");
+        console.log("d");
+      });
 
 })
