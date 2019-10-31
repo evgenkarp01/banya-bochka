5715f67bb4a42dc93e80ea0f3d4176b9dc29c7ee
ewal@list.ru <ewal@list.ru>
2019-10-31 16:30:41 +0300

    31102019

--- js/index.js
+++ js/index.js
@@ -6,4 +6,59 @@ $( document ).ready(function() {
         console.log("d");
       });
 
+     $(function(){
+        $('[name="phone"]').mask("+7(999) 999-99-99");
+    });
+
+    function call(ev) {
+ 	  var msg   = $(ev).closest('form').serialize();
+        $.ajax({
+          type: 'POST',
+          url: '../php/send.php',
+          data: msg,
+          success: function(data) {
+            $(ev).closest('.result').html(data);
+            $(ev).slideUp('fast');
+          },
+          error:  function(xhr, str){
+	    alert('Возникла ошибка: ' + xhr.responseCode);
+          }
+        });
+    }
+        
+    $("#modal-button").on("click", function(event){
+        event.preventDefault();
+         
+         if(($('[name="phone"]').val()!='')&&($('[name="phone"]').val()!='+7(___)___-__-__')){
+            call(this);
+         }else{
+         	$('[name="phone"]').addClass('error');
+         }
+    });
+
+
+    $('.popup-with-form').magnificPopup({
+		type: 'inline',
+		preloader: false,
+		focus: '#name',
+		callbacks: {
+			beforeOpen: function() {
+				if($(window).width() < 700) {
+					this.st.focus = false;
+				} else {
+					this.st.focus = '#name';
+				}
+			}
+		}
+	});
+
+	$('.popup-youtube').magnificPopup({
+		disableOn: 700,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+		fixedContentPos: false
+	});
+
 })
